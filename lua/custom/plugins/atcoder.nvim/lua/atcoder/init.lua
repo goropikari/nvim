46003eab3ac6717ba6ea89a6a4fc68cfc66a823c
goropikari <goropikari56@gmail.com>
2024-12-10 03:46:19 +0900

    fix output path

--- lua/custom/plugins/atcoder.nvim/lua/atcoder/init.lua
+++ lua/custom/plugins/atcoder.nvim/lua/atcoder/init.lua
@@ -1,8 +1,15 @@
 local M = {}
 
+---@class Config
+---@field out_dirpath string
+
+---@type Config
 local default_config = {
   out_dirpath = '/tmp/atcoder/',
 }
+
+---@type Config
+---@diagnostic disable-next-line
 local config = {}
 
 vim.keymap.set('n', '<leader>WW', function()
@@ -15,14 +22,14 @@ vim.keymap.set('n', '<leader>WW', function()
     'make',
     'test',
     'INPUT=' .. vim.fn.expand('%:a'),
-    'OUTPUT=' .. config.outpath .. vim.fn.expand('%:t:r'),
+    'OUTPUT=' .. config.out_dirpath .. vim.fn.expand('%:t:r'),
   }, {}, function(out)
     vim.print(out.stdout)
     vim.schedule(function()
       vim.cmd('messages')
     end)
   end)
-end, { noremap = true, silent = true, desc = '競プロ: Save file and yank entire file' })
+end, { noremap = true, silent = true, desc = '競プロ: Save file and copy' })
 
 function M.setup(opts)
   config = vim.tbl_deep_extend('force', default_config, opts or {})
