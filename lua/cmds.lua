186a7c0ca4882c782ed49f001471b3901312c0b3
goropikari <goropikari56@gmail.com>
2024-12-10 02:46:10 +0900

    refac for competitive programming

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -3,14 +3,25 @@ local api = vim.api
 -- [[ 競技プログラミング用 ]]
 api.nvim_create_user_command('Make', function()
   vim.cmd('messages clear')
-  print(vim.fn.system({ 'make', 'test', 'ARGS=' .. vim.fn.expand('%:r') }))
-  vim.cmd('messages')
+  local outpath = '/tmp/competititve_programming/'
+  vim.fn.mkdir(outpath, 'p')
+  vim.system({
+    'make',
+    'test',
+    'INPUT=' .. vim.fn.expand('%:a'),
+    'OUTPUT=' .. outpath .. vim.fn.expand('%:t:r'),
+  }, {}, function(out)
+    vim.print(out.stdout)
+    vim.schedule(function()
+      vim.cmd('messages')
+    end)
+  end)
 end, {})
 
 vim.keymap.set('n', '<leader>WW', function()
   vim.cmd('write') -- ファイルを保存
-  vim.cmd('%y') -- ファイル全体をヤンク
   vim.cmd('Make')
+  vim.cmd('%y') -- ファイル全体をヤンク
 end, { noremap = true, silent = true, desc = '競プロ: Save file and yank entire file' })
 
 api.nvim_create_user_command('LiveServer', function(opts)
