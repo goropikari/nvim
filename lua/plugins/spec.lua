b99193f5617f469f1ff9dc8a8483393ef82db338
goropikari <goropikari56@gmail.com>
2025-08-01 10:21:28 +0900

    remove noice, add termlist

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -266,61 +266,61 @@ return {
       },
     },
   },
-  {
-    -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
-    -- command が pop up window で表示される
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- "rcarriga/nvim-notify",
-    },
-    keys = {
-      {
-        '<leader>nh',
-        function()
-          require('noice').cmd('history')
-        end,
-        desc = 'Noice History',
-      },
-      {
-        '<leader>ne',
-        function()
-          require('noice').cmd('error')
-        end,
-        desc = 'Noice Error',
-      },
-      {
-        '<leader>nl',
-        function()
-          require('noice').cmd('last')
-        end,
-        desc = 'Noice Last',
-      },
-    },
-  },
+  -- {
+  --   -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
+  --   -- command が pop up window で表示される
+  --   'folke/noice.nvim',
+  --   event = 'VeryLazy',
+  --   opts = {
+  --     lsp = {
+  --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+  --       override = {
+  --         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --         ['vim.lsp.util.stylize_markdown'] = true,
+  --         ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+  --       },
+  --     },
+  --     -- you can enable a preset for easier configuration
+  --     presets = {
+  --       bottom_search = false, -- use a classic bottom cmdline for search
+  --       command_palette = true, -- position the cmdline and popupmenu together
+  --       long_message_to_split = false, -- long messages will be sent to a split
+  --       inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --       lsp_doc_border = false, -- add a border to hover docs and signature help
+  --     },
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     'MunifTanjim/nui.nvim',
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     -- "rcarriga/nvim-notify",
+  --   },
+  --   keys = {
+  --     {
+  --       '<leader>nh',
+  --       function()
+  --         require('noice').cmd('history')
+  --       end,
+  --       desc = 'Noice History',
+  --     },
+  --     {
+  --       '<leader>ne',
+  --       function()
+  --         require('noice').cmd('error')
+  --       end,
+  --       desc = 'Noice Error',
+  --     },
+  --     {
+  --       '<leader>nl',
+  --       function()
+  --         require('noice').cmd('last')
+  --       end,
+  --       desc = 'Noice Last',
+  --     },
+  --   },
+  -- },
   {
     -- buffer を tab で表示する
     'romgrk/barbar.nvim',
@@ -862,13 +862,13 @@ return {
     --   },
     -- },
     keys = {
-      {
-        '<c-t>',
-        function()
-          vim.cmd('ToggleTerm')
-        end,
-        mode = { 'n', 't' },
-      },
+      -- {
+      --   '<c-t>',
+      --   function()
+      --     vim.cmd('ToggleTerm')
+      --   end,
+      --   mode = { 'n', 't' },
+      -- },
       {
         '<leader>st',
         function()
@@ -1219,7 +1219,7 @@ return {
         desc = 'add hugo content',
       },
       {
-        '<leader>ho',
+        '<leader>hs',
         function()
           vim.system({ 'hugo', 'server', '-D' }, {
             text = true,
@@ -1239,6 +1239,23 @@ return {
           })
         end,
         desc = 'hugo server',
+      },
+    },
+  },
+  {
+    'goropikari/termlist.nvim',
+    dependencies = {
+      'akinsho/toggleterm.nvim',
+    },
+    dev = true,
+    opts = {},
+    keys = {
+      {
+        '<c-t>',
+        function()
+          require('termlist').toggle()
+        end,
+        mode = { 'n', 't' },
       },
     },
   },
